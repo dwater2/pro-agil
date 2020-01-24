366b6a77b0fc0897b54f7ba9dbfc2d5a97c6c635
Donizete Waterkemper <dwater2@gmail.com>
2020-01-24 00:03:49 -0300

    Angular - Interpolação,

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,12 +8,42 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any = [];
-  constructor(private http: HttpClient) { 
+  _textoFiltrado: string;
+  get textoFiltrado(): string {
+    return this._textoFiltrado;
+  }
+  set textoFiltrado(value: string){
+    this._textoFiltrado = value;
+    this.eventosFiltrados = this.textoFiltrado ? this.filtrarEventos(this.textoFiltrado) : this.eventos;
   }
 
+  eventosFiltrados: any = [];
+  eventos: any = [];
+  imagemLargura = 30;
+  imagemMargem = 2;
+  mostrarImagem = false;
+  alternarTextoBtn = "Mostrar Img";
+
+  constructor(private http: HttpClient) {
+  }
+
+  filtrarEventos(filtrarPor : string) : any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
   ngOnInit() {
     this.getEventos();
+  }
+
+  alternarImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
+    if(this.mostrarImagem){
+      this.alternarTextoBtn = "Ocultar Img";
+    }else{
+      this.alternarTextoBtn = "Mostrar Img";
+    }
   }
 
   getEventos(){
